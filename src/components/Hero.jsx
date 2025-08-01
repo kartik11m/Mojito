2d7c5b858e7497c45d123bab9e67316aaf706e21
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-02 01:32:53 +0530

    Hero section created

---
+++ src/components/Hero.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+        <section id='hero' className="noisy">
+            <h1 className='title'>Mojito</h1>
+
+            <img src="/images/hero-left-leaf.png" alt="left-leaf" className="left-leaf"/>
+            <img src="/images/hero-right-leaf.png" alt="right-leaf" className="right-leaf"/>
+            <div className='body'>
+                <div className="content">
+                    <div className="space-y-5 hidden md:block"></div>
+                    <p>Cool. Crisp. Classic.</p>
+                    <p className='subtitle'>
+                        Sip the Spirit <br /> of Summer
+                    </p>
+                    <div className='view-cocktails'>
+                        <p className="subtitle">
+                            Every cocktail on our menu is a blend of premium ingredients, 
+                            creative flair, and timeless recipes — designed to delight your senses. 
+                        </p>
+                        <a href="#cocktails">View Cocktails</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Hero

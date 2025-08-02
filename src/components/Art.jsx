db05f552b16e5e2edb4b6cb3a99526e4d86ba55e
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-03 03:12:37 +0530

    adding animation using GSAP

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -6,7 +6,28 @@ import { useGSAP } from '@gsap/react';
 
 const Art = () => {
 
-   
+    const isMobile = useMediaQuery({maxWidth : 767});
+
+    useGSAP(() => {
+        const start = isMobile ? 'top 20%' : 'top top';
+
+        const maskTimeline = gsap.timeline({
+            scrollTrigger:{
+                trigger: '#art',
+                start,
+                end: 'bottom center',
+                scrub: 1.5,
+                // adding 1.5 delay
+                pin : true,
+                // this will keep the art section fixed at place while scrolling through animation
+            }
+        })
+
+        maskTimeline
+        .to('.will-fade' , {opacity: 0 , stragger: 0.2, ease: 'power1.inOut',})
+        .to('.masked-img' , {scale: 1.3 , maskPosition: 'center' , maskSize: '400%', duration: 1 , ease:'power1.inOut'})
+        .to('#masked-content' , { opacity: 1 , duration: 1 , ease: 'power1.inOut'})
+    })
 
   return (
 	<div id="art">

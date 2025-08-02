43e5d0ab22a02b6cdc751b8dab64bfa69116ac0f
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-03 01:41:35 +0530

    adding animation using GSAP to text and images

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,30 @@
 import React from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { SplitText } from 'gsap/all'
 
 const About = () => {
+    useGSAP(() => {
+        const titleSplit = SplitText.create('#about h2' , {
+            type: 'words'
+        })
+
+        const scrollTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#about',
+                start: 'top center'
+            }
+        })
+
+        scrollTimeline
+        .from(titleSplit.words , {
+            opacity: 0 , duration: 1 , yPercent: 100, ease: 'expo.out', stagger: 0.02
+        })
+        .from('.top-grid div, .bottom-grid div' , {
+            opacity:0 , duration: 1 , ease: 'power1.inOut', stagger: 0.04 , 
+        }, '-=0.5')
+        // it will make the animation start half a second before the previous animation gets ended
+    })
   return (
     <div id="about">
         <div className="mb-16 md:px-0 px-5">

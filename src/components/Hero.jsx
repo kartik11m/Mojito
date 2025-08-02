b8f9e17eaaf9ba4ac7769bd2c9eaedf7c5fca6af
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-02 23:50:25 +0530

    animating the video while scrolling using GSAP

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React , {useRef} from 'react'
 import gsap from 'gsap'
+import { useMediaQuery } from 'react-responsive'
 import { useGSAP } from '@gsap/react'
 import { SplitText } from 'gsap/all'
 
 const Hero = () => {
+
+    const videoRef = useRef();
+    const isMobile = useMediaQuery({maxWidth : 767})
+
     useGSAP(() =>{
         const heroSplit = new SplitText('.title' , {type: 'chars , words'}); // for animating the title with chars by chars , words by words
         const paragraphSplit = new SplitText('.subtitle' , {type : 'lines'}); // for animating the subtitle line by line
-        heroSplit.chars.forEach((char) => char.classList.add('text-gradient')) // changing the style of Mojito
+        heroSplit.chars.forEach((char) => char.classList.add('text-gradient')) // Apply text-gradient class once before animating
 
         gsap.from(heroSplit.chars , {
             yPercent: 100,
@@ -40,6 +45,30 @@ const Hero = () => {
     .to('.right-leaf', {y:200} , 0)
     .to('.left-leaf',{y: -200} , 0)
 
+    const startValue = isMobile ? 'top 50%' : 'center 60%';
+    const endValue = isMobile ? '120% top' : 'bottom top';
+
+    // video animation timeline
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: 'video',
+            start: startValue,
+            end: endValue,
+            scrub:true,
+            pin: true,
+            // it will stuck the video when you scroll
+            // onEnter: () => videoRef.current.play(),
+            // onEnterBack : () => videoRef.current.play(),
+            // onLeave: () => videoRef.current.pause(),
+            // onLeaveBack: () => videoRef.current.pause,
+        }
+    })
+
+    videoRef.current.onloadedmetadata = () => {
+	 tl.to(videoRef.current, {
+		currentTime: videoRef.current.duration,
+	 });
+    }
 
     },[])
 
@@ -67,6 +96,19 @@ const Hero = () => {
                 </div>
             </div>
         </section>
+
+        <div className="video absolute inset-0">
+            <video
+            ref={videoRef}
+             muted 
+             playsInline 
+             preload='auto'
+             src="/videos/output.mp4" 
+            //  autoPlay
+            // loop
+             >
+             </video>
+        </div>
     </>
   )
 }
